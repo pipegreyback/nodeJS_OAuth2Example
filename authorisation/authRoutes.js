b08fbfea302580c4dc4cd1ee166dc3d975c3c93a
Felipe Esteban Condon Silva <felipe.condon.13@sansano.usm.cl>
2017-12-28 15:34:01 -0300

    Added some routes
    Routes for resource server added

--- authorisation/authRoutes.js
+++ authorisation/authRoutes.js
@@ -5,6 +5,7 @@ module.exports =  (router, expressApp, authRoutesMethods) => {
 
     //route for allowing existing users to login
     router.post('/login', expressApp.oauth.grant(), authRoutesMethods.login)
-
+		router.post('/get_retrive_token', expressApp.oauth.grant(), authRoutesMethods.login)
+		router.post('/accesToResource', expressApp.oauth.grant(), authRoutesMethods.login)
     return router
 }
